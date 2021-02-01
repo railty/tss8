312ae4d7b4da74b6c32e4907d30c3c5afab130f7
shawn ning <zxning@gmail.com>
2021-02-01 14:52:45 -0500

    update

--- sync.js
+++ sync.js
@@ -1,4 +1,6 @@
 const logger = require('electron-log');
+const ffs = require('final-fs');
+const MD5 = require("crypto-js/md5");
 const mysql = require('mysql2/promise');
 const path = require("path");
 const admin = require("firebase-admin");
@@ -71,8 +73,8 @@ async function test_mysql(){
 
 //test_mysql();
 
-async function dlPunches(storeId){
-    logger.log(`dlPunches for ${storeId}`);
+async function syncPunches(storeId){
+    logger.log(`syncPunches for ${storeId}`);
     const conn = await mysql.createConnection(configSync.mysql);
     let db = admin.firestore();
     let bucket = admin.storage().bucket();
@@ -114,10 +116,124 @@ async function dlPunches(storeId){
     logger.log(`total ${count} punches downloaded`);
 }
 
+async function syncEmployees(){
+    logger.log(`syncEmployees`);
+    const conn = await mysql.createConnection(configSync.mysql);
+    let fb = admin.firestore();
+    let bucket = admin.storage().bucket();
+
+    let employeesFb = {};
+    let files;
+    let count = 0;
+
+    async function uploadPhoto(id, dataUrl){
+        console.log(`uploading ${id}`);
+
+        let photo_file = `${configSync.employeesPhotoPath}/${id}.jpg`;
+        let photoData = await ffs.readFile(photo_file);
+        let gcf = bucket.file(`employees/${id}.jpg`);
+
+        await gcf.save(photoData);
+
+        let url = gcf.publicUrl();
+        return url;
+    }
+    
+    async function updateEmployee(employeeRef, employee){
+        console.log("updating " + employee.id);
+        count = count + 1;
+        await employeeRef.set(employee);
+    }
+    
+    async function imgToDataUrl(photo_file){
+        //no encoding means binary
+        let photoData = await ffs.readFile(photo_file);
+        let buf = Buffer.from(photoData, 'binary');
+        let string = buf.toString('base64');
+        string = "data:image/jpeg;base64," + string;
+        return string;
+    }
+    
+    async function syncEmployee(empDb){
+        let id = empDb.id;
+        let photo_file = `${configSync.employeesPhotoPath}/${id}.jpg`;
+    
+        let dataUrl = await imgToDataUrl(photo_file);
+        let photo_md5;
+        let photo_url;
+
+        let empFb = employeesFb[id];
+
+        //if local have a photo
+        if (dataUrl){
+          photo_md5 = MD5(dataUrl).toString();
+   
+          //have remote
+          if (empFb){
+            //photo not match
+            if (empFb.photo_md5 != photo_md5) {
+              photo_url = await uploadPhoto(id, dataUrl);
+            }
+            else{
+              console.log("skip same photo" + id);  
+            }
+          }
+          else {
+              //the photo might be alright, just the data missing, but neverthenless, upload the photo
+              photo_url = await uploadPhoto(id, dataUrl);
+          }
+        }
+    
+        let employeeRef = fb.collection("employees").doc(id.toString());
+    
+        //photo changed, update fb anyway
+        if (photo_url) {
+          empDb.photo_url = photo_url;
+          empDb.photo_md5 = photo_md5;
+          logger.log("update " + id + "for new photo");
+          await updateEmployee(employeeRef, empDb);
+        }
+        else{
+            empDb.photo_url = empFb.photo_url;
+            empDb.photo_md5 = empFb.photo_md5;
+
+            if (empFb){
+                if (empFb.updated_at.toMillis() < empDb.updated_at.valueOf()) {
+                    logger.log("update " + id + "for new updated_at");
+                    await updateEmployee(employeeRef, empDb);
+                }
+                else{
+                    logger.log("skip fb data, same timestamp " + id);
+                }
+            }
+            else{
+                logger.log("create " + id);
+                await updateEmployee(employeeRef, empDb);
+            }
+        }
+    }
+
+    let [rows, cols] = await conn.execute('select * from tss.employees');
+
+    let employeesSnapshot = await fb.collection("employees").get();
+    employeesSnapshot.forEach((doc) => {employeesFb[doc.id] = doc.data();});
+
+    for (let emp of rows){
+        let {store_id, empno, name, id, barcode, name_cn, department, address, city, postal, rate, active, active2, notes, position, created_at, updated_at} = {...emp};
+        emp = {store_id, empno, name, id, barcode, name_cn, department, address, city, postal, rate, active, active2, notes, position, created_at, updated_at};
+        await syncEmployee(emp);
+    }
+    
+    await conn.end();
+    logger.log(`total ${count} employees uploaded`);
+}
+
 async function sync(){
     for (let store of configSync.stores ){
-        let punches = await dlPunches(store);
+        //await syncPunches(store);
     }
+
+    await syncEmployees();
 }
 
 async function main(){
@@ -126,7 +242,7 @@ async function main(){
     config = await loadConfig(appPath, configPath);
 
     await sync();
-    setInterval(await sync, configSync.interval);
+    //setInterval(await sync, configSync.interval);
 }
 
 main();
